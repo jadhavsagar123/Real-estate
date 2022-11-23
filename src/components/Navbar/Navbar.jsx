e08286dea870a44ef689b7bbb2f6c43449291f99
jadhavsagar123 <99315933+jadhavsagar123@users.noreply.github.com>
2022-11-23 21:25:15 +0530

    responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,100 +1,60 @@
+import { useState } from "react";
+import "./navbar.css";
 import { NavLink } from "react-router-dom";
-import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
-    <div className={styles.navbar}>
-     <div className={styles.start}>
-        <div className={styles.estatery}>
-          <NavLink
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "red",
-                    background: "rgb(40, 77, 179)",
-                    textDecoration: "none",
-                  }
-                : {
-                    color: "white",
-                    background: "rgb(40, 77, 179)",
-                    textDecoration: "none",
-                  }
-            }
-            to="/"
-          >
-            Estatery
-          </NavLink>
-        </div>
-        <div className={styles.favorite}>
-          <NavLink
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "red",
-                    background: "rgb(40, 77, 179)",
-                    textDecoration: "none",
-                  }
-                : {
-                    color: "white",
-                    background: "rgb(40, 77, 179)",
-                    textDecoration: "none",
-                  }
-            }
-            to="/favorites"
-          >
-            Favorites
-          </NavLink>
-          </div>
+    <nav style={{ backgroundColor: "blue" }} className="navigation">
+      <NavLink to="/" className="brand-name">
+        Estatery
+      </NavLink>
+      <NavLink to="/favorites" className="brand-name">
+        Favorites
+      </NavLink>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/#">Buy</a>
+          </li>
+          <li>
+            <a href="/#">Rent</a>
+          </li>
+          <li>
+            <a href="/#">Sell</a>
+          </li>
+          <li>
+            <a href="/#">Manage-property</a>
+          </li>
+          <li>
+            <a href="/#">Resources</a>
+          </li>
+        </ul>
       </div>
-      <div className={styles.center}>
-        <div className={styles.buy}>
-          <button>Buy</button>
-        </div>
-        <div className={styles.rent}>
-          <button>Rent</button>
-        </div>
-        <div className={styles.sell}>
-          <button>Sell</button>
-        </div>
-        <div className={styles.manageproperty}>
-          <select defaultValue={"Manage Property"}>
-            <option>Manage Property</option>
-            <option>Mumbai</option>
-          </select>
-        </div>
-        <div className={styles.resources}>
-          <select defaultValue={"Resources"}>
-            <option>Resources</option>
-            <option>Mumbai</option>
-          </select>
-          </div>
-      </div>
-      <div className={styles.end}>
-      <div className={styles.login}>
-        <button style={{
-              backgroundColor: '#6777D3',
-              borderColor: '#6777D3',
-              color: 'white',
-              height: '50px',
-              width: '110px',
-              borderRadius: '5px',
-              fontSize: '18px',
-            }}>Login</button>
-      </div>
-      <div className={styles.signup}>
-        <button style={{
-              backgroundColor: 'red',
-              borderColor: '#6777D3',
-              color: 'white',
-              height: '50px',
-              width: '110px',
-              borderRadius: '5px',
-              fontSize: '18px',
-            }}>SignUp</button>
-        </div>
-      </div>
-    </div>
+    </nav>
   );
-};
-
-export default Navbar;
+}
