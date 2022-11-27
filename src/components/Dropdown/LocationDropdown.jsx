a601f1d7d0a65f41dcc7f77423a0909676153690
jadhavsagar123 <99315933+jadhavsagar123@users.noreply.github.com>
2022-11-27 17:18:10 +0530

    filter responsive

--- src/components/Dropdown/LocationDropdown.jsx
+++ src/components/Dropdown/LocationDropdown.jsx
@@ -1,7 +1,7 @@
 const LocationDropdown = ({ selectLocationHandler }) => {
   return (
     <>
-      <select onChange={selectLocationHandler} defaultValue={'pune'}>
+      <select onChange={selectLocationHandler}  defaultValue={'pune'}>
         <option value='pune'>Pune</option>
         <option value='mumbai'>Mumbai</option>
       </select>
